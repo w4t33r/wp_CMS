525de77c20712b692676b021ee0eaac132d2df2d
w4t33r <vk.educate@protonmail.ch>
2023-09-11 11:12:56 -0700

    template/post part

---
+++ template-parts/content-post.php
@@ -0,0 +1,26 @@
+<div class="blog-post">
+    <img src="images/blog/blog-lg.jpg" alt="" class="img-fluid">
+    <div class="mt-4 mb-3 d-flex">
+        <div class="post-author mr-3">
+            <i class="fa fa-user"></i>
+            <span class="h6 text-uppercase"><?php the_author(); ?></span>
+        </div>
+
+        <div class="post-info">
+            <i class="fa fa-calendar-check"></i>
+            <span><?php the_date(); ?></span>
+        </div>
+    </div>
+
+    <a href="#" class="h4 "><?php the_title(); ?></a>
+     <?php the_content(); ?>
+    <div class="my-4">
+        <h5 class="d-inline-block mr-3">Поделитесь:</h5>
+        <ul class="list-inline d-inline-block">
+            <li class="list-inline-item"><a href="#"><i class="fab fa-facebook"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fab fa-linkedin"></i></a></li>
+        </ul>
+    </div>
+</div>
