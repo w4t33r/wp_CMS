c1626e467764b439483765f660bded499d0bb90c
w4t33r <vk.educate@protonmail.ch>
2023-09-11 12:20:50 -0700

    code fixes

--- header.php
+++ header.php
@@ -4,11 +4,6 @@
     <!-- Required meta tags -->
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    <meta name="description" content="seo & digital marketing"/>
-    <meta
-        name="keywords"
-        content="marketing,digital marketing,creative, agency, startup,promodise,onepage, clean, modern,seo,business, company"
-    />
     <?php wp_head(); ?>
 </head>
 <body data-spy="scroll" data-target=".fixed-top">
