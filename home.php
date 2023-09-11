ac0d808e06781a74463ffe0bb66d9c99243fe7a1
w4t33r <vk.educate@protonmail.ch>
2023-09-11 10:00:59 -0700

    changed function for post, fixed css

--- home.php
+++ home.php
@@ -19,40 +19,71 @@
         <div class="row">
             <div class="col-lg-8">
                 <div class="row">
-                    <?php if (have_posts()) : while (have_posts()) :
-                        the_post(); ?>
-                        <div class="col-lg-6">
-                            <div class="blog-post">
-                                <?php
-                                if( has_post_thumbnail() ) {
-                                    the_post_thumbnail('medium', array('class' => "img-fluid"));
-                                }
-                                else {
-                                    echo '<img class="img-fluid" src="'.get_template_directory_uri().'/wp-content/themes/barb_group7/images/blog/blog-3.jpg" />';
-                                }
-                                ?>
-                                <img src="images/blog/blog-1.jpg" alt="" class="img-fluid">
-                                <div class="mt-4 mb-3 d-flex">
-                                    <div class="post-author mr-3">
-                                        <i class="fa fa-user"></i>
-                                        <span class="h6 text-uppercase"><?php the_author(); ?></span>
-                                    </div>
+                    <?php $count = 0;
+                    if (have_posts()) : while (have_posts()) : the_post();
+                        $count++;
+                        switch ($count) {
+                            case "3": ?>
+                                <div class="col-lg-12">
+                                    <div class="blog-post">
+                                        <?php
+                                        if (has_post_thumbnail()) {
+                                            the_post_thumbnail('medium', array('class' => "img-fluid w-100"));
+                                        } else {
+                                            echo '<img class="img-fluid w-100" src="' . get_template_directory_uri() . '/wp-content/themes/barb_group7/images/blog/blog-3.jpg" />';
+                                        }
+                                        ?>
+                                        <img src="images/blog/blog-1.jpg" alt="" class="img-fluid w-100">
+                                        <div class="mt-4 mb-3 d-flex">
+                                            <div class="post-author mr-3">
+                                                <i class="fa fa-user"></i>
+                                                <span class="h6 text-uppercase"><?php the_author(); ?></span>
+                                            </div>
 
-                                    <div class="post-info">
-                                        <i class="fa fa-calendar-check"></i>
-                                        <span><?php the_date(); ?></span>
+                                            <div class="post-info">
+                                                <i class="fa fa-calendar-check"></i>
+                                                <span><?php the_time('j F Y'); ?></span>
+                                            </div>
+                                        </div>
+                                        <a class="h4 "><?php the_title(); ?></a>
+                                        <p class="mt-3"><?php the_excerpt(); ?></p>
+                                        <a href="<?php echo get_the_permalink(); ?>" class="read-more">Read Article <i
+                                                    class="fa fa-angle-right"></i></a>
                                     </div>
                                 </div>
-                                <a href="blog-single.html" class="h4 "><?php the_title(); ?></a>
-                                <p class="mt-3"><?php the_excerpt(); ?></p>
-                                <a href="<?php echo get_the_permalink(); ?>" class="read-more">Read Article <i
-                                            class="fa fa-angle-right"></i></a>
-                            </div>
-                        </div>
-                    <?php endwhile; else: ?>
+                            <?php
+                                break;
+                            default: ?>
+                                <div class="col-lg-6">
+                                    <div class="blog-post">
+                                        <?php
+                                        if (has_post_thumbnail()) {
+                                            the_post_thumbnail('medium', array('class' => "img-fluid w-100"));
+                                        } else {
+                                            echo '<img class="img-fluid w-100" src="' . get_template_directory_uri() . '/wp-content/themes/barb_group7/images/blog/blog-3.jpg" />';
+                                        }
+                                        ?>
+                                        <img src="images/blog/blog-1.jpg" alt="" class="img-fluid w-100">
+                                        <div class="mt-4 mb-3 d-flex">
+                                            <div class="post-author mr-3">
+                                                <i class="fa fa-user"></i>
+                                                <span class="h6 text-uppercase"><?php the_author(); ?></span>
+                                            </div>
+
+                                            <div class="post-info">
+                                                <i class="fa fa-calendar-check"></i>
+                                                <span><?php the_time('j F Y'); ?></span>
+                                            </div>
+                                        </div>
+                                        <a class="h4 "><?php the_title(); ?></a>
+                                        <p class="mt-3"><?php the_excerpt(); ?></p>
+                                        <a href="<?php echo get_the_permalink(); ?>" class="read-more">Read Article <i
+                                                    class="fa fa-angle-right"></i></a>
+                                    </div>
+                                </div>
+                            <?php break; } endwhile; else: ?>
                         No Articles.
                     <?php endif; ?>
-
                 </div>
             </div>
         </div>
